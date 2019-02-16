24b8d30ffa21147f62b7c175e2df15a7f38d877d
celsoernani <celsoernane@gmail.com>
2019-02-16 01:27:16 -0300

    Show search resolv

--- teste.py
+++ teste.py
@@ -1,5 +1,8 @@
 import math
 
+#______________________________________________________________________________
+# Missionaries and Cannibals Problem
+
 class State():
 	def __init__(self, cannibalLeft, missionaryLeft, boat, cannibalRight, missionaryRight):
 		self.cannibalLeft = cannibalLeft
@@ -37,52 +40,62 @@ def successors(cur_state):
 	if cur_state.boat == 'left':
 		new_state = State(cur_state.cannibalLeft, cur_state.missionaryLeft - 2, 'right',
                                   cur_state.cannibalRight, cur_state.missionaryRight + 2)
+		## Two missionaries cross left to right.
 		if new_state.is_valid():
 			new_state.parent = cur_state
 			children.append(new_state)
 		new_state = State(cur_state.cannibalLeft - 2, cur_state.missionaryLeft, 'right',
                                   cur_state.cannibalRight + 2, cur_state.missionaryRight)
+		## Two cannibals cross left to right.
 		if new_state.is_valid():
 			new_state.parent = cur_state
 			children.append(new_state)
 		new_state = State(cur_state.cannibalLeft - 1, cur_state.missionaryLeft - 1, 'right',
                                   cur_state.cannibalRight + 1, cur_state.missionaryRight + 1)
+		## One missionary and one cannibal cross left to right.
 		if new_state.is_valid():
 			new_state.parent = cur_state
 			children.append(new_state)
 		new_state = State(cur_state.cannibalLeft, cur_state.missionaryLeft - 1, 'right',
                                   cur_state.cannibalRight, cur_state.missionaryRight + 1)
+		## One missionary crosses left to right.
 		if new_state.is_valid():
 			new_state.parent = cur_state
 			children.append(new_state)
 		new_state = State(cur_state.cannibalLeft - 1, cur_state.missionaryLeft, 'right',
                                   cur_state.cannibalRight + 1, cur_state.missionaryRight)
+		## One cannibal crosses left to right.
 		if new_state.is_valid():
 			new_state.parent = cur_state
 			children.append(new_state)
 	else:
 		new_state = State(cur_state.cannibalLeft, cur_state.missionaryLeft + 2, 'left',
                                   cur_state.cannibalRight, cur_state.missionaryRight - 2)
+		## Two missionaries cross right to left.
 		if new_state.is_valid():
 			new_state.parent = cur_state
 			children.append(new_state)
 		new_state = State(cur_state.cannibalLeft + 2, cur_state.missionaryLeft, 'left',
                                   cur_state.cannibalRight - 2, cur_state.missionaryRight)
+		## Two cannibals cross right to left.
 		if new_state.is_valid():
 			new_state.parent = cur_state
 			children.append(new_state)
 		new_state = State(cur_state.cannibalLeft + 1, cur_state.missionaryLeft + 1, 'left',
                                   cur_state.cannibalRight - 1, cur_state.missionaryRight - 1)
+		## One missionary and one cannibal cross right to left.
 		if new_state.is_valid():
 			new_state.parent = cur_state
 			children.append(new_state)
 		new_state = State(cur_state.cannibalLeft, cur_state.missionaryLeft + 1, 'left',
                                   cur_state.cannibalRight, cur_state.missionaryRight - 1)
+		## One missionary crosses right to left.
 		if new_state.is_valid():
 			new_state.parent = cur_state
 			children.append(new_state)
 		new_state = State(cur_state.cannibalLeft + 1, cur_state.missionaryLeft, 'left',
                                   cur_state.cannibalRight - 1, cur_state.missionaryRight)
+		## One cannibal crosses right to left.
 		if new_state.is_valid():
 			new_state.parent = cur_state
 			children.append(new_state)
@@ -119,65 +132,13 @@ def print_solution(solution):
 			print ("(" + str(state.cannibalLeft) + "," + str(state.missionaryLeft) \
                               + "," + state.boat + "," + str(state.cannibalRight) + "," + \
                               str(state.missionaryRight) + ")")
-#FORÇA BRUTA COM LISTAS -> TODO IMPLEMNTAR SEM SER SOMA, MAS COM APPEND E REMOVE IN LISTS
-def brute_force():
-    mission = [1,1,1]
-    canibais = [2,2,2]
-    barco = []
-    margem1 = [mission, canibais]
-    margem2 = []
-    barco = [mission[0], canibais[0]]
-    margem1[0].remove(1)
-    margem1[1].remove(2)
-    margem2.append(barco[1])
-    barco.remove(2)
-    margem1[0].append(barco[0])
-    barco.remove(1)
-    barco.append(margem1[1][0])
-    barco.append(margem1[1][0])
-    margem1[1].remove(2)
-    margem1[1].remove(2)
-    margem2.append(barco[0])
-    barco.remove(2)
-    margem1[1].append(barco[0])
-    barco.remove(2)
-    barco.append(margem1[0][0])
-    barco.append(margem1[0][1])
-    margem1[0].remove(1)
-    margem1[0].remove(1)
-    margem2.append(barco[0])
-    margem2.remove(2)
-    barco.remove(1)
-    barco.append(margem2[0])
-    margem1[1].append(barco[1])
-    barco.remove(2)
-    barco.append(margem1[0][0])
-    margem1[0].remove(1)
-    margem2.append(barco[0])
-    margem2.append(barco[1])
-    barco.remove(1)
-    barco.remove(1)
-    barco.append(margem2[0])
-    margem2.remove(2)
-    barco.append(margem1[1][0])
-    margem1[1].remove(2)
-    margem2.append(barco[0])
-    barco.remove(2)
-    barco.append(margem1[1][0])
-    margem1[1].remove(2)
-    margem2.append(barco[0])
-    margem2.append(barco[1])
-    barco.remove(2)
-    barco.remove(2)
-    print("Atualmente a margem1 contem: " , margem1)
-    print("Atualmente a margem2 contem: " , margem2)
-    print("Atualmente o barco contem: " , barco)
-
 
 def main():
 	solution = breadth_first_search()
+	print ("Missionaries and Cannibals solution:")
+	print ("(cannibalLeft,missionaryLeft,boat,cannibalRight,missionaryRight)")
 	print_solution(solution)
-    # brute_force()
+
+# if called from the command line, call main()
 if __name__ == "__main__":
     main()
-
